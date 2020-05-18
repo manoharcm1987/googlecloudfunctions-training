9dc02e2e1bcae351a9f10477978f17f654d3126e
manoharcm1987 <manoharcm1987@gmail.com>
2020-05-18 16:54:33 +0530

    Update index.js

--- helloworld-http/index.js
+++ helloworld-http/index.js
@@ -5,5 +5,8 @@
  * @param {Object} res Cloud Function response context.
  */
 exports.helloGET = (req, res) => {
-  res.send('Hello World!');
+  let minVer = process.env.MIN_VER;
+  let maxVer = process.env.MAX_VER;
+  let hostName = process.env.HOSTNAME;
+  res.status(200).send("Function version:" + maxVer + "." + minVer);
 };
